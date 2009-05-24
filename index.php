19bc84413c1dbfa6db65695455fe70a4ac64cafe
Pieter Kokx <pieter@kokx.nl>
2009-05-24 20:56:03 +0200

    Added title.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-define('VERSION', '1.0.0');
+define('VERSION', '1.0.1');
 
 /**
  * Root location
@@ -27,12 +27,20 @@ require_once 'Zend/Loader/Autoloader.php';
 $autoloader = Zend_Loader_Autoloader::getInstance();
 $autoloader->registerNamespace('Kokx_');
 
+// view initialization
 $view = new Zend_View();
 
 $view->setScriptPath(ROOT . DIRECTORY_SEPARATOR . 'views');
 
 $view->script = '';
 $view->report = '';
+$view->title  = '';
+
+// default options
+$view->options = array(
+    'middleText' => 'Na het gevecht...',
+    'hideTime'   => true
+);
 
 // convert the CR if it is there
 if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['report'])) {
@@ -46,20 +54,51 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['report'])) {
         exit($view->render('layout.phtml'));
     }
 
-    // use Zend View to render the shit
+    // we are using Zend_View to render the shit
 
     // now set all the variables
     $view->time   = $parser->getTime();
     $view->rounds = $parser->getRounds();
     $view->result = $parser->getResult();
 
-    // default options
-    $view->options = array(
-        'middleText' => 'Na het gevecht...',
-        'hideTime'   => true
-    );
-    
-    // check for options
+    /**
+     * Generate the title
+     */
+
+    // attackers first
+    $attackers = array();
+
+    foreach ($view->rounds[0]['attackers'] as $attacker) {
+        $attackers[] = $attacker['player']['name'];
+    }
+
+    $attackers = array_keys(array_flip($attackers));
+
+    // now defenders
+    $defenders = array();
+
+    foreach ($view->rounds[0]['defenders'] as $defender) {
+        $defenders[] = $defender['player']['name'];
+    }
+
+    $defenders = array_keys(array_flip($defenders));
+
+    // ViRuS & kokx vs rasta & joop (A: 55.000.000, V: 88.000.000) [TOT: 20.000.000]
+
+    $view->result['totallosses'] = $view->result['attackerlosses'] + $view->result['defenderlosses'];
+
+    $title  = implode(' & ', $attackers) . ' vs. ' . implode(' & ', $defenders);
+    $title .= ' (A: ' . number_format($view->result['attackerlosses'], 0, ',', '.');
+    $title .= ', V: ' . number_format($view->result['defenderlosses'], 0, ',', '.') . ')';
+    $title .= ' [TOT: ' . number_format($view->result['totallosses'], 0, ',', '.') . ']';
+
+    $view->title = $title;
+
+
+    /**
+     * Check and use the options
+     */
+
     if (isset($_POST['middletext']) && is_string($_POST['middletext'])) {
         $view->options['middleText'] = $_POST['middletext'];
     }
