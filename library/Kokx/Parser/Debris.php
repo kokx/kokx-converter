fa1ef15e2fdd431072c32241b7375dcf1c7b8f16
Pieter Kokx <pieter@kokx.nl>
2009-05-29 17:03:24 +0200

    Added debris report parsing.

---
+++ library/Kokx/Parser/Debris.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Ogame Converter
+ *
+ * @category   Kokx
+ * @package    Kokx_Parser
+ */
+
+/**
+ * CR parser
+ *
+ * @category   Kokx
+ * @package    Kokx_Parser
+ */
+class Kokx_Parser_Debris
+{
+
+    /**
+     * The harvest reports
+     *
+     * @var array
+     */
+    protected $_harvest = array();
+
+
+    /**
+     * Get the harvest reports
+     *
+     * @return array
+     */
+    public function getHarvest()
+    {
+        return $this->_harvest;
+    }
+
+    /**
+     * Parse a harvest report
+     *
+     * @param string $source
+     *
+     * @return Kokx_Parser_Debris
+     */
+    public function parse($source)
+    {
+        /**
+         * Example report:
+         *
+         * Je 8001 recyclers hebben een totale opslagcapaciteit van 101.264.259.
+         * In het bestemmingsveld zweven 0 metaal en 0 kristal in de ruimte.
+         * Je hebt 0 metaal en 0 kristal opgehaald.
+         */
+        $regex  = 'Je ([0-9.]*?) recyclers hebben een totale opslagcapaciteit van ([0-9.]*?). ';
+        $regex .= 'In het bestemmingsveld zweven ([0-9.]*?) metaal en ([0-9.]*?) kristal in de ruimte. ';
+        $regex .= 'Je hebt ([0-9.]*?) metaal en ([0-9.]*?) kristal opgehaald.';
+
+        $matches = array();
+
+        preg_match_all('/' . $regex . '/i', $source, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $this->_harvest[] = array(
+                'recs'         => (int) str_replace('.', '', $match[1]),
+                'storage'      => (int) str_replace('.', '', $match[2]),
+                'fieldmetal'   => (int) str_replace('.', '', $match[3]),
+                'fieldcrystal' => (int) str_replace('.', '', $match[4]),
+                'metal'        => (int) str_replace('.', '', $match[5]),
+                'crystal'      => (int) str_replace('.', '', $match[6])
+            );
+        }
+
+        return $this;
+    }
+}
