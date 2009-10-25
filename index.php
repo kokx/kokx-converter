0ea9614c4dc217862cd4c6e47bf0e46c50b20189
Kokx <pieter@kokx.nl>
2009-10-25 22:32:00 +0100

    Added GPLv3 license.

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
+/**
+ * This file is part of Kokx's CR Converter.
+ *
+ * Kokx's CR Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kokx's CR Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kokx's CR Converter.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category   KokxConverter
+ * @license    http://www.gnu.org/licenses/gpl.txt
+ * @copyright  Copyright (c) 2009 Kokx
+ * @package    Index
+ */
 
 define('VERSION', '1.3.0');
+define('LINK', 'http://converter.kokx.nl/');
+
+define('GOOGLE_AD_CLIENT', 'pub-2117246455436779');
+define('GOOGLE_AD_SLOT', '0492312042');
+define('GOOGLE_GA_TRACKER_ID', 'UA-9087312-1');
 
 /**
  * Root location
